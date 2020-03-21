8edb1eb7a030499007fa9f479e4edf6ee8d212fb
datsu <atsu34412@gmail.com>
2020-03-22 00:35:53 +0900

    first commit

---
+++ app/src/main/java/com/example/counterexp2/FristFragment.kt
@@ -0,0 +1,19 @@
+package com.example.counterexp2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.view.*
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+        view.button.setOnClickListener {
+            findNavController().navigate(R.id.action_first_to_second)
+        }
+        return view
+    }
+}
